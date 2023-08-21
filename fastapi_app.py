891f02c6727f539abbddda1f926e30cd3e066129
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2023-08-21 22:52:42 +0530

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+# Create a FastAPI app instance.
 app = FastAPI()
 
 # Load the pipeline model
@@ -13,14 +14,17 @@ pipeline_filename = 'email_spam_pipeline.pkl'
 with open(pipeline_filename, 'rb') as pipeline_file:
     clf = pickle.load(pipeline_file)
 
+# Define a Pydantic BaseModel class to represent the request input data.
 class Item(BaseModel):
     text: str
 
+# Create an API endpoint that listens to POST requests at "/predict/".
 @app.post("/predict/")
 async def predict(item: Item):
+    # Extract the input text from the request.
     text = item.text
-    prediction = clf.predict([text])[0]
-    probability = clf.predict_proba([text])[0]
+    prediction = clf.predict([text])[0]  #If no [0], output= [1]/[0], get rid of array with this
+    probability = clf.predict_proba([text])[0]  #Similaryly with this, if not [0], output = [[0.2,0.8]]
     return {"prediction": prediction, "probability": probability[prediction]}
 
 if __name__ == "__main__":
